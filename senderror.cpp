e279a0b78b5d89fede3a9bb54515bea85cbea630
Colin Steifel <csteifel@gmail.com>
2013-03-14 17:48:58 -0400

    Finished sending error packets

--- senderror.cpp
+++ senderror.cpp
@@ -1,6 +1,22 @@
 #include "senderror.h"
 
 
-void sendError(int sockfd, sockaddr_storage sendErrTo, socklen_t sendErrToLen, std::string errorString){
-	sendto(sockfd, errorString.c_str(), errorString.length(), 0, (struct sockaddr *) &sendErrTo, sendErrToLen);
+void sendError(int sockfd, sockaddr_storage sendErrTo, socklen_t sendErrToLen, unsigned short errorCode, std::string errorString){
+	unsigned int bufSize = 2 + 2 + errorString.length() + 1;
+	char * errorBuffer = (char *) malloc(bufSize);
+	unsigned short opcode = htons(5);
+	errorCode = htons(errorCode);
+
+	//Zero everything out and then copy over all the information we need to send
+	memset(errorBuffer, 0, bufSize);
+
+	memcpy(errorBuffer, &opcode, 2);
+	memcpy(errorBuffer+2, &errorCode, 2);
+	memcpy(errorBuffer+4, errorString.c_str(), errorString.length());
+
+	if(sendto(sockfd, errorBuffer, bufSize, 0, (struct sockaddr *) &sendErrTo, sendErrToLen) == -1){
+		std::cerr << "Error sending error packet!";
+	}
+	//Done with the socket since there was an error
+	close(sockfd);
 }
--- senderror.h
+++ senderror.h
@@ -1,9 +1,13 @@
 #ifndef SENDINGERROR_H
 #define SENDINGERROR_H
 
+#include <iostream>
 #include <string>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
 #include <sys/socket.h>
 
-void sendError(int, sockaddr_storage, socklen_t, std::string);
+void sendError(int, sockaddr_storage, socklen_t, unsigned short, std::string);
 
 #endif
