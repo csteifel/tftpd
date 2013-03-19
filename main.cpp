c8270dd402e32cce267e8fa7e8707fd695c24fd7
Colin Steifel <csteifel@gmail.com>
2013-03-19 15:04:23 -0400

    Changed try catch loop and also changed filename to a pointer to be freed by the callee

--- main.cpp
+++ main.cpp
@@ -32,20 +32,20 @@ int main(void){
 		int readAmount;
 		//Statically allocated buffer, will be able to fit any of the initial connection messages
 		char buffer[512];
-		short opcode = 0;
+		uint16_t opcode = 0;
 		//Max length for file names is 255 on linux
-		char filename[256];
+		char * filename = (char *) malloc(256);
 		char * afterFilename;
 		//Forcing 6 because we are only worrying about octet/binary mode
-		char mode[6];
+		char mode[7];
 		int clientFd;
 
 
 		socklen_t comingFromLen = sizeof(struct sockaddr_storage);
 
 		memset(buffer, 0, 512);
-		memset(filename, 0, 255);
-		memset(mode, 0, 6);
+		memset(filename, 0, 256);
+		memset(mode, 0, 7);
 
 		readAmount = recvfrom(serverFD, buffer, 512, 0, (struct sockaddr *) &comingFrom, &comingFromLen);
 
@@ -63,28 +63,33 @@ int main(void){
 		//Find the occurence of the first \0 character know that the mode is after that
 		afterFilename = (char *) rawmemchr(buffer+2, '\0');
 
-		strncpy(mode, afterFilename+1, 5);
+		strncpy(mode, afterFilename+1, 6);
 
-		if(strcmp(mode, "octet") != 0){
+		if(!(strcmp(mode, "octet") != 0 || strcmp(mode, "binary") != 0)){
 			//Unsupported transfer type send back an error packet.
-			
 			continue;
 		}
 
 		//Got information from request packet
 		//Now create a new socket to respond to the request
-
 		clientFd = socket(comingFrom.ss_family, SOCK_DGRAM, 0);
+		
+		try{
+			if(opcode == 1){
+				//Asking for a file so we must send a file to them
+				std::thread sendingThread(sendFile, clientFd, comingFrom, comingFromLen, filename);
+				sendingThread.detach();
+			}else if(opcode == 2){
+				//Giving us a file so we must check if we can take it
+				std::cerr << "Receiving\n";
+			}else{
+				//Not a read or write request so send back an error
+				std::thread sendErrorThread(sendError, clientFd, comingFrom, comingFromLen, 4, "Illegal TFTP operation.");
+				sendErrorThread.detach();
+			}
 
-		if(opcode == 1){
-			//Asking for a file so we must send a file to them
-			std::thread sendingThread(sendFile, clientFd, comingFrom, comingFromLen, filename);//sendFile(clientFd, comingFrom, comingFromLen, filename));
-		}else if(opcode == 2){
-			//Giving us a file so we must check if we can take it
-			std::cerr << "Receiving\n";
-		}else{
-			//Not a read or write request so send back an error
-			sendError(clientFd, comingFrom, comingFromLen, "Not a valid request\n");
+		}catch(const std::system_error& e){
+			std::cerr << e.what();
 		}
 	}
 
